534cafef017d071a04b83b582316047b10d80be3
manulathealmighty <93851899+manulathealmighty@users.noreply.github.com>
2021-12-13 21:46:09 +0530

    Update test_env_vars.sh

--- test_env_vars.sh
+++ test_env_vars.sh
@@ -1,5 +1,5 @@
 #This is a templaet example of what an env script should look lik
-export BOT_TOKEN="650879477:AAFO_o2_nt2gmwA-h0TeIo4bSqI-WLxp6VM"
+export BOT_TOKEN="2108250215:AAGGzngYNdOWg3wF3_EjrRz-2kZLLZVJZ14"
 export LOG_LEVEL="debug"
 export POSTGRES_USER="test_user"
 export POSTGRES_PASS="test_pass"
